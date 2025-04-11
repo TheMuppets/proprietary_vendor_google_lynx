d1ac2c733ab14f10aa6372313e5235ea794c062f
Michael Bestas <mkbestas@lineageos.org>
2025-04-11 20:32:32 +0300

    lynx: Import blobs & firmware from BP1A.250405.007.B1
    Change-Id: I986fd7db4dbaa6d6d17e111a08a92b7ca3d01267

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),lynx)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,b8210010aefef1f85fb6253d56de47d05d961bcb)
-$(call add-radio-file-sha1-checked,radio/bl1.img,9f065e185c9f5c3eb588b035be8801ae88111313)
-$(call add-radio-file-sha1-checked,radio/bl2.img,73194093832641ee11b3c1a22f6866b3630421a6)
-$(call add-radio-file-sha1-checked,radio/bl31.img,34f1850f50deee9439940031bcae4078289dbf08)
-$(call add-radio-file-sha1-checked,radio/gsa.img,9f377241aee57823d8a4e8c51d6a12bf8c871060)
-$(call add-radio-file-sha1-checked,radio/ldfw.img,928598cf10d6e53f057dffb3ad96675aa65dd23e)
+$(call add-radio-file-sha1-checked,radio/abl.img,4632f2c4915075bf821fba59be54dd8b82651e2d)
+$(call add-radio-file-sha1-checked,radio/bl1.img,7ce0fb3fa1ded55aa049848d46835a505dc713c0)
+$(call add-radio-file-sha1-checked,radio/bl2.img,c3bd6844debf966164bd967ace7f3a24dc56d310)
+$(call add-radio-file-sha1-checked,radio/bl31.img,1f9073988db4de4d47dfec255f79f8288033060d)
+$(call add-radio-file-sha1-checked,radio/gsa.img,43186e2eed5de981067b7059e928eb1d343b9e51)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,d3f876f9a2b04606cee1aa948f129dbaa02a8d11)
 $(call add-radio-file-sha1-checked,radio/modem.img,05204be817636abf06a02bd4124b502afc0861db)
-$(call add-radio-file-sha1-checked,radio/pbl.img,ec5c075bab922ca16d3125e0f5aef01777603335)
-$(call add-radio-file-sha1-checked,radio/tzsw.img,6aa40cd307129b28f6e41982a11b73a01546a0d8)
+$(call add-radio-file-sha1-checked,radio/pbl.img,62eb9f7f00e618ecbc242643518cda71032bd99c)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,69150c355db8a413cd98f2d2f6ef3125537ffd19)
 
 endif
