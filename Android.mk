d382d24bb26f0c2956550f373e7b6b1c16694091
Michael Bestas <mkbestas@lineageos.org>
2025-11-22 13:02:30 +0200

    lynx: Update to BP3A.251105.015
    Change-Id: I70b0dd51d198ea2c77761238df93cea868a30b5e

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@ $(call add-radio-file-sha1-checked,radio/bl2.img,5ef8b01e05c89950ec00bf04f9a2e02
 $(call add-radio-file-sha1-checked,radio/bl31.img,34196f59f8707d95bcb0c198104a733e2a0e3ace)
 $(call add-radio-file-sha1-checked,radio/gsa.img,c8c52311e8fa282f8776d7c00ba2bcb194112969)
 $(call add-radio-file-sha1-checked,radio/ldfw.img,9caafcec50733c8ac8402edff8497fc77634ddf6)
-$(call add-radio-file-sha1-checked,radio/modem.img,4a4a23b0364478a6ced0e126ff707c6c86c3b1f0)
+$(call add-radio-file-sha1-checked,radio/modem.img,e97bebb652a42156caf888f66c6cbd8544b9bd84)
 $(call add-radio-file-sha1-checked,radio/pbl.img,426b4a6f7729edeace9611e2641bc9a9a1f6bfed)
 $(call add-radio-file-sha1-checked,radio/tzsw.img,dc1a76ccc1af2a34de65209d5b95d32d8b2c954a)
 
