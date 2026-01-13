b4191548128e68631b94b97d35d86aa098a45fb5
Michael Bestas <mkbestas@lineageos.org>
2026-01-13 19:04:27 +0200

    lynx: Update to BP4A.260105.004.E1
    Change-Id: Iade157f6a9861ff647d97b10549e19182d8ce563

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),lynx)
 
-$(call add-radio-file-sha1-checked,radio/abl.img,28b046d95982df43e4633d0371ebaac743b65900)
-$(call add-radio-file-sha1-checked,radio/bl1.img,af09dfa274919ecbbf5627a20e2dfce93bd636b5)
-$(call add-radio-file-sha1-checked,radio/bl2.img,5ef8b01e05c89950ec00bf04f9a2e021e8b5170a)
-$(call add-radio-file-sha1-checked,radio/bl31.img,34196f59f8707d95bcb0c198104a733e2a0e3ace)
-$(call add-radio-file-sha1-checked,radio/gsa.img,c8c52311e8fa282f8776d7c00ba2bcb194112969)
-$(call add-radio-file-sha1-checked,radio/ldfw.img,9caafcec50733c8ac8402edff8497fc77634ddf6)
-$(call add-radio-file-sha1-checked,radio/modem.img,e97bebb652a42156caf888f66c6cbd8544b9bd84)
-$(call add-radio-file-sha1-checked,radio/pbl.img,426b4a6f7729edeace9611e2641bc9a9a1f6bfed)
-$(call add-radio-file-sha1-checked,radio/tzsw.img,dc1a76ccc1af2a34de65209d5b95d32d8b2c954a)
+$(call add-radio-file-sha1-checked,radio/abl.img,90c56645464695e44e8f6048d62b4d9fcacbee2e)
+$(call add-radio-file-sha1-checked,radio/bl1.img,774b12d80488e68ca282bc3cfcb34337c023512b)
+$(call add-radio-file-sha1-checked,radio/bl2.img,df248577641b27276965eee72f29c9c0389d19d7)
+$(call add-radio-file-sha1-checked,radio/bl31.img,e968badbd9b78b13cec265562fd60757ebe3abb6)
+$(call add-radio-file-sha1-checked,radio/gsa.img,853b9d9dd7cf2ef780aee5eb3c108357246e1784)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,4edc48e52137338802e3dffa3582a45d60067b0f)
+$(call add-radio-file-sha1-checked,radio/modem.img,92207f66f5b1cdbc8530c800d704425349d7a2aa)
+$(call add-radio-file-sha1-checked,radio/pbl.img,648b09e373a639abab4820ae137b25c149d361e8)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,13667edc5e3da80b03e40e261dc5337129fdfbce)
 
 endif
